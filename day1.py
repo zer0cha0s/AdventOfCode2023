69d01e4f5dc42960e133e0068bff0bc6ae4635c5
Shane H <shumpleby1988@gmail.com>
2023-12-01 14:13:17 +0000

    Day 1

---
+++ day1.py
@@ -0,0 +1,38 @@
+import re
+
+# read input file
+file = open('./input.txt').read()
+
+# Set vars
+total = 0
+result = 0
+
+for line in file.split('\n'):
+    digits = []
+
+    for val in line:
+        if val.isdigit():
+            digits.append(val)
+    
+    total = int(digits[0]+digits[-1])
+    result += total
+
+print('Part 1:', result)
+
+# Reset variables so they can be used again
+result = 0
+total = 0
+
+for line in file.split('\n'): 
+    digits = []
+    
+    for num,val in enumerate(line):
+        if val.isdigit():
+            digits.append(val)
+        for d,val in enumerate(['one','two','three','four','five','six','seven','eight','nine']):
+            if line[num:].startswith(val):
+                digits.append(str(d+1))
+    total = int(digits[0]+digits[-1])
+    result += total
+
+print('Part 2:', result)
